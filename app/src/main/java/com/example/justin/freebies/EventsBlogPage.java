b79206275ee1c33b1e2adbe54221018788b3717b
Sirpeter89 <just2rules@gmail.com>
2018-03-10 18:08:03 -0800

    Half of blogging

--- app/src/main/java/com/example/justin/freebies/EventsBlogPage.java
+++ app/src/main/java/com/example/justin/freebies/EventsBlogPage.java
@@ -7,10 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-public class EventsPage extends AppCompatActivity {
+public class EventsBlogPage extends AppCompatActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,24 +17,28 @@ public class EventsPage extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch(item.getItemId()) {
                 case R.id.navigation_home:
-                    Intent intent = new Intent(EventsPage.this, MainPage.class);
+                    Intent intent = new Intent(EventsBlogPage.this, MainPage.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                     startActivity(intent);
                     overridePendingTransition(0,0);
                     break;
 
                 case R.id.navigation_events:
+                    Intent intent1 = new Intent(EventsBlogPage.this, EventsPage.class);
+                    intent1.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent1);
+                    overridePendingTransition(0,0);
                     break;
 
                 case R.id.navigation_map:
-                    Intent intent2 = new Intent(EventsPage.this, MapPage.class);
+                    Intent intent2 = new Intent(EventsBlogPage.this, MapPage.class);
                     intent2.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                     startActivity(intent2);
                     overridePendingTransition(0,0);
                     break;
 
                 case R.id.navigation_account:
-                    Intent intent3 = new Intent(EventsPage.this, AccountPage.class);
+                    Intent intent3 = new Intent(EventsBlogPage.this, AccountPage.class);
                     intent3.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                     startActivity(intent3);
                     overridePendingTransition(0,0);
@@ -46,28 +48,34 @@ public class EventsPage extends AppCompatActivity {
         }
     };
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_events_page);
-
-        Button eventButton = (Button)findViewById(R.id.eventButton);
-        eventButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent select = new Intent(getApplicationContext(), EventsBlogPage.class);
-                startActivity(select);
-            }
-        });
+        setContentView(R.layout.activity_events_blog_page);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         Menu menu = navigation.getMenu();
         MenuItem menuItem = menu.getItem(1);
         menuItem.setChecked(true);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 
+        getMenuInflater().inflate(R.menu.blogmenu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.blog_menu)
+        {
+            startActivity(new Intent(EventsBlogPage.this, PostActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/justin/freebies/PostActivity.java
@@ -0,0 +1,83 @@
+package com.example.justin.freebies;
+
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class PostActivity extends AppCompatActivity {
+
+    private ImageButton selectImage;
+    private EditText mTitleBox;
+    private EditText mDescriptionBox;
+
+    private Button mSubmitPostButton;
+
+    private Uri mImageUri = null;
+
+    private static final int Gallery_Req = 1;
+
+    private StorageReference store;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
+        store = FirebaseStorage.getInstance().getReference()
+
+        selectImage = (ImageButton) findViewById(R.id.imageSelect);
+
+        mTitleBox = (EditText) findViewById(R.id.TitleBox);
+        mDescriptionBox = (EditText) findViewById(R.id.DescriptionBox);
+
+        mSubmitPostButton = (Button) findViewById(R.id.SubmitPostButton);
+
+        selectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallery = new Intent(Intent.ACTION_GET_CONTENT);
+                gallery.setType("image/*");
+                startActivityForResult(gallery, Gallery_Req);
+            }
+        });
+
+        mSubmitPostButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startPosting();
+            }
+        });
+    }
+
+    private void startPosting() {
+        String title_val = mSubmitPostButton.getText().toString().trim();
+        String desc_val = mDescriptionBox.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && mImageUri != null){
+
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == Gallery_Req && resultCode == RESULT_OK){
+            mImageUri = data.getData();
+            selectImage.setImageURI(mImageUri);
+        }
+
+    }
+}
