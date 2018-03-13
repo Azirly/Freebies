f83d83ed1ffbc335ca9ef38d0efdafd9882965fe
Ray Hsu <wyhsu@uci.edu>
2018-03-12 23:55:56 -0700

    Changed it to skip the login screen for faster testing + other in description
    Changed up how post activity works, currently broken again

--- app/src/main/java/com/example/justin/freebies/PostActivity.java
+++ app/src/main/java/com/example/justin/freebies/PostActivity.java
@@ -5,10 +5,13 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.media.Image;
 import android.net.Uri;
+import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.provider.SyncStateContract;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -17,7 +20,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -27,6 +32,9 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -39,12 +47,14 @@ public class PostActivity extends AppCompatActivity {
     private Button mSubmitPostButton;
 
     private Uri mImageUri = null;
+    private String mCurrentPhotoPath;
 
-    private static final int Gallery_Req = 1;
     private static final int REQUEST_IMAGE_CAPTURE = 111;
+    private static final int CAMERA_REQUEST_CODE = 1;
 
     private StorageReference store;
     private DatabaseReference database;
+    private StorageReference filepath;
 
     private ProgressDialog progress;
 
@@ -53,6 +63,44 @@ public class PostActivity extends AppCompatActivity {
     private double currentLong;
     private Date currentTime;
 
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File...
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.justin.freebies",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,14 +124,19 @@ public class PostActivity extends AppCompatActivity {
         takeImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                progress.setMessage("Getting info...");
+                progress.show();
                 if(gps.canGetLocation()) {
-                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    //Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    progress.setMessage("Getting info...");
+                    progress.show();
                     currentLat = gps.getLatitude();
                     currentLong = gps.getLongitude();
                     currentTime = Calendar.getInstance().getTime();
-                    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    dispatchTakePictureIntent();
+                    /*if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
                         startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                    }
+                    }*/
                 }
             }
         });
@@ -106,7 +159,7 @@ public class PostActivity extends AppCompatActivity {
 
         if(!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && mImageUri != null){
 
-            StorageReference filepath = store.child("Blog_Images").child(mImageUri.getLastPathSegment());
+            //StorageReference filepath = store.child("Blog_Images").child(mImageUri.getLastPathSegment());
 
             filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
@@ -136,10 +189,26 @@ public class PostActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            progress.setMessage("Uploading...");
+            progress.show();
             mImageUri = data.getData();
             takeImage.setImageURI(mImageUri);
-        }
 
+            //StorageReference filepath = store.child("Blog_Images").child(mImageUri.getLastPathSegment());
+            filepath = store.child("Blog_Images").child(mImageUri.getLastPathSegment());
+            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(PostActivity.this, "Upload Successful!", Toast.LENGTH_SHORT).show();
+                    progress.dismiss();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(PostActivity.this, "Upload Failed!", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 
 }
